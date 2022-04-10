89261df129204f65a044f3098552d278097b9f11
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2022-04-10 15:54:15 +0200

    fix: do not rebuild everything every time

--- build-web.sh
+++ build-web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rustup target add wasm32-unknown-unknown && \
-cargo build --target wasm32-unknown-unknown && \
-cargo install -f wasm-bindgen-cli && \
-wasm-bindgen --out-dir target/generated/ --web target/wasm32-unknown-unknown/debug/visitlab-wgpu.wasm && \
-python3 -m http.server
+    cargo build --target wasm32-unknown-unknown && \
+    cargo install wasm-bindgen-cli && \
+    wasm-bindgen --out-dir target/generated/ --web target/wasm32-unknown-unknown/debug/visitlab-wgpu.wasm && \
+    python3 -m http.server
