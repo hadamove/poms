2c374b3e3cc49373f56006a860157468f9a164fa
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2022-04-21 14:12:32 +0200

    script: refactor build-electron.sh script

--- scripts/build-electron.sh
+++ scripts/build-electron.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 SOURCE_DIR=`dirname "$0"`
-TARGET_DIR=${SOURCE_DIR}/../target
 ELECTRON_DIR=${SOURCE_DIR}/../src/electron
 
-rustup target add wasm32-unknown-unknown && \
-    cargo build --target wasm32-unknown-unknown --features legacy-shader && \
-    cargo install wasm-bindgen-cli && \
-    wasm-bindgen --out-dir ${TARGET_DIR}/generated/ --web ${TARGET_DIR}/wasm32-unknown-unknown/debug/visitlab-wgpu.wasm && \
-    cd ${ELECTRON_DIR} && npm i && (npm start&) && cd ../.. && \
-    python3 -m http.server
+source ${SOURCE_DIR}/build-web.sh &
+server_pid=$!
+
+cd ${ELECTRON_DIR}
+npm install
+npm start
+
+kill $server_pid
