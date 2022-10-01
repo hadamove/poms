7f12359138a6735424435f25f63346c1eeeae476
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2022-10-01 19:56:13 +0200

    feat: update gui

--- src/gui/my_app.rs
+++ src/gui/my_app.rs
@@ -1,23 +1,41 @@
 #![allow(dead_code)]
 
-#[derive(Default)]
 pub struct MyApp {
-    pub colors: ColorWidgets,
     pub file_to_load: Option<String>,
+
+    pub ses_resolution: u32,
+    pub render_spacefill: bool,
+    pub render_ses_surface: bool,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            file_to_load: None,
+            ses_resolution: 160,
+            render_spacefill: true,
+            render_ses_surface: false,
+        }
+    }
 }
 
 impl epi::App for MyApp {
     fn name(&self) -> &str {
-        "Atom colors"
+        "Molecular Surface Explorer"
     }
 
     fn update(&mut self, ctx: &epi::egui::Context, _frame: &epi::Frame) {
-        // egui::containers::Window::new(self.name())
-        //     .vscroll(true)
-        //     .hscroll(true)
-        //     .show(ctx, |ui| {
-        //         self.colors.ui(ui);
-        //     });
+        egui::containers::Window::new("Settings").show(ctx, |ui| {
+            ui.add(egui::Slider::new(&mut self.ses_resolution, 10..=160).text("SES resolution"));
+            ui.add(egui::Checkbox::new(
+                &mut self.render_spacefill,
+                "Render Spacefill",
+            ));
+            ui.add(egui::Checkbox::new(
+                &mut self.render_ses_surface,
+                "Render SES surface",
+            ));
+        });
 
         egui::TopBottomPanel::top("menu_bar").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
@@ -36,66 +54,6 @@ impl epi::App for MyApp {
                     }
                 });
             });
-        });
-    }
-}
-
-#[derive(PartialEq)]
-pub struct ColorWidgets {
-    pub h_color: [f32; 4],
-    pub c_color: [f32; 4],
-    pub o_color: [f32; 4],
-    pub n_color: [f32; 4],
-    pub s_color: [f32; 4],
-}
-
-impl Default for ColorWidgets {
-    fn default() -> Self {
-        ColorWidgets {
-            h_color: [1.0, 1.0, 1.0, 1.0],
-            c_color: [0.2, 0.2, 0.2, 1.0],
-            o_color: [1.0, 0.0, 0.0, 1.0],
-            n_color: [0.0, 0.0, 1.0, 1.0],
-            s_color: [1.0, 1.0, 0.0, 1.0],
-        }
-    }
-}
-
-impl ColorWidgets {
-    fn ui(&mut self, ui: &mut egui::Ui) {
-        egui::reset_button(ui, self);
-
-        let Self {
-            h_color,
-            c_color,
-            o_color,
-            n_color,
-            s_color,
-        } = self;
-
-        ui.horizontal(|ui| {
-            ui.color_edit_button_rgba_unmultiplied(h_color);
-            ui.label("Hydrogen".to_string());
-        });
-
-        ui.horizontal(|ui| {
-            ui.color_edit_button_rgba_unmultiplied(c_color);
-            ui.label("Carbon".to_string());
-        });
-
-        ui.horizontal(|ui| {
-            ui.color_edit_button_rgba_unmultiplied(o_color);
-            ui.label("Oxygen".to_string());
-        });
-
-        ui.horizontal(|ui| {
-            ui.color_edit_button_rgba_unmultiplied(n_color);
-            ui.label("Nitrogen".to_string());
-        });
-
-        ui.horizontal(|ui| {
-            ui.color_edit_button_rgba_unmultiplied(s_color);
-            ui.label("Sulfur".to_string());
         });
     }
 }
